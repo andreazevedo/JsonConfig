ab97702684e25f9dc317bf5d340681aa5af41c2a
Andre Azevedo <andre.azevedo@gmail.com>
2012-03-29 18:31:48 -0300

    Sample web project.

---
+++ samples/web/JsonConfigWebTest/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Web.Mvc;
+using JsonConfig;
+
+namespace JsonConfigWebTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var sb = new StringBuilder();
+            sb.Append("name: " + JsonConfigManager.DefaultConfig.name + "<br />");
+            sb.Append("host: " + JsonConfigManager.DefaultConfig.host + "<br />");
+            sb.Append("port: " + JsonConfigManager.DefaultConfig.port + "<br />");
+
+            return Content(sb.ToString());
+        }
+
+    }
+}
