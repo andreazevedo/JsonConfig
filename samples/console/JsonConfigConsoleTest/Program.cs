46835828ab47dea16502a10c1fbade84115fcec5
Andre Azevedo <andre.azevedo@gmail.com>
2012-03-29 18:22:14 -0300

    Sample console app

---
+++ samples/console/JsonConfigConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JsonConfig;
+
+namespace JsonConfigConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("name: {0}", JsonConfigManager.DefaultConfig.name);
+            Console.WriteLine("host: {0}", JsonConfigManager.DefaultConfig.host);
+            Console.WriteLine("port: {0}", JsonConfigManager.DefaultConfig.port);
+
+            Console.ReadKey();
+        }
+    }
+}
